0c1dcbb4f8c6ea72a8ce2a8b436d7f7755001eb6
jsimpso81 <60305400+jsimpso81@users.noreply.github.com>
2024-11-12 16:24:43 -0500

    Adventure - decus release 11340 version

---
+++ src/advent_decus_11340/advent_decus_11340.f90
@@ -0,0 +1,25 @@
+!  advent_decus_11340.f90 
+!
+!  FUNCTIONS:
+!  advent_decus_11340 - Entry point of console application.
+!
+
+!****************************************************************************
+!
+!  PROGRAM: advent_decus_11340
+!
+!  PURPOSE:  Entry point for the console application.
+!
+!****************************************************************************
+
+    program advent_decus_11340
+
+    implicit none
+
+    ! Variables
+
+    ! Body of advent_decus_11340
+    print *, 'Hello World'
+
+    end program advent_decus_11340
+
